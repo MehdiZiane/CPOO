6d4efe9fc7b71005c86cbcb8c3b9ad401101d731
MehdiZiane <mehdi.ziane@outlook.fr>
2023-01-31 20:19:08 +0100

    exo_2 list quick sort

--- intro_liste/exo_2.py
+++ intro_liste/exo_2.py
@@ -1,13 +1,20 @@
 def main():
     L = [43, -37, 67, 89, 790]
     L.sort()
-    print(L)
-    number = input("enter a number: ")
-    for i in L:
-        if i == int(number):
-            print("congratulation the number is in the list")
-            break
-    print("try again")
- 
+    borneinf = 0
+    bornesup = len(L) - 1
+    value = input("enter a number pls: ")
+    
+    while borneinf <= bornesup:
+        mid = (borneinf + bornesup) //2
+        if L[mid] < int(value):
+            borneinf = mid + 1
+        elif L[mid] > int(value):
+            bornesup = mid - 1
+        else :
+            print("congratulation u found the number ")
+            return True
+    print("your number is not in the list")
+
 if __name__=="__main__":
     main()
