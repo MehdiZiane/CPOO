0a90e14487760324207b98938eb1063dba2bcba6
MehdiZiane <mehdi.ziane@outlook.fr>
2023-02-10 16:39:35 +0100

    last fonction

--- intro_dico/exo_1.py
+++ intro_dico/exo_1.py
@@ -1,18 +1,21 @@
 import pprint
-
-def Age(dct):
-    pprint.pprint(dct)
+from datetime import datetime
+def age(dct):
+    today = datetime.now()
+    birthdate = datetime.strptime(dct['date'],'%d/%m/%Y')
+    age = today.year - birthdate.year
+    return age
 
 def main():
-    first_dico = {'nom' : 'Dupont', 'prenom' : 'Davi', 'age' : 30}
-    for i, j in first_dico.items():
+    first_dct = {'nom' : 'Dupont', 'prenom' : 'Davi', 'age' : 30}
+    for i, j in first_dct.items():
         if i =="prenom":
-            first_dico[i] = 'David'
-    print(list(first_dico.items()))
-    print("{0[prenom]:} {0[nom]:} a {0[age]:} ans".format(first_dico))
-    if 'date' not in first_dico:
-        first_dico['date'] = '17/01/1991'
-    pprint.pprint(first_dico)
-    Age(first_dico)
+            first_dct[i] = 'David'
+    print(list(first_dct.items()))
+    print("{0[prenom]:} {0[nom]:} a {0[age]:} ans".format(first_dct))
+    if 'date' not in first_dct:
+        first_dct['date'] = '17/01/1991'
+    pprint.pprint(first_dct)
+    print(age(first_dct))
 if __name__ == "__main__":
     main()
